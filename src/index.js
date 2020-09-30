f024b0ed7c857bf07c6ed3fe3a542142284231f9
bigkastus <61244432+bigkastus@users.noreply.github.com>
2020-09-30 22:17:02 +0300

    feat: add solutions-2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }  
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }  
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum = 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }    
+    for ( i=0; i<array.length; i++){                
+        if(i !== (array.length - 1)){
+            sum = sum + array[i];             
+        }     
+        else {
+            sum = (sum + array[array.length-1])/(array.length);
+        }
+       
+    }
+    return sum;
 }
